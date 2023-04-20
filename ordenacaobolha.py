5b6841d26a43518d1bb2907f534fd1a62117beb1
Felipe Ricardo <85915186+felipricardo@users.noreply.github.com>
2023-04-19 21:27:33 -0300

    Update ordenacaobolha.py

--- ordenacaobolha.py
+++ ordenacaobolha.py
@@ -9,7 +9,7 @@ for i in range(0,4):
     for j in range(i+1,5):
         print("/t j:", j)
         if lista[i] > lista[j]:
-            aux = lista[i]
-            lista[i] = lista[j]
+            aux = lista[i] 
+            lista[i] = lista[j] # = lista[j], lista[i]  somente em python e sem a variavel "aux"
             lista[j] = aux
 print(lista)
